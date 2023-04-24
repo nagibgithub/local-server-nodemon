6adbe53525e4dbddfef0dceb5fcfe9d053cfbedd
Nagib Mahfuz <nmfuad.bd@gmail.com>
2023-04-24 21:30:06 +0600

    creat local server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const jsonData = require('./data/category.json')
+
+app.get('/', (req, res) => {
+    res.send('this is live local server')
+})
+
+app.get('/data', (req, res) => {
+    res.send(jsonData)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${ port }`)
+})
