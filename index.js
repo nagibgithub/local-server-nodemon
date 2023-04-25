805b65d412ca4c60cd9b038bb569e895d4b2ea69
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-04-25 13:25:04 +0600

    add news api

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
-const jsonData = require('./data/category.json');
+const newsData = require('./data/news.json')
+const categoryData = require('./data/category.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('this is live local server');
 });
 
-app.get('/data', (req, res) => {
-    res.send(jsonData);
+app.get('/categories', (req, res) => {
+    res.send(categoryData);
 });
+
+app.get('/news', (req, res) => {
+    res.send(newsData); 
+});
+
+app.get('/news/:newsId', (req, res) => {
+    const id = req.params.newsId
+    const idNews = newsData.find(pd => pd._id === id)
+    res.send(idNews)
+});
+
+app.get('/categories/:categoryId', (req, res) => {
+    const id = req.params.categoryId
+    const categoryNews = newsData.filter(pd => pd.category_id === id)
+    res.send(categoryNews)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${ port }`);
